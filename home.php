c975a2fcca4b6fb3e71d5e1d66291b9cfc2e4c3a
kev7862 <kevmatite7862@gmail.com>
2021-09-21 17:35:01 +0300

    Created a homepage landingpage where users will be redirected after successful login

--- home.php
+++ home.php
@@ -10,10 +10,19 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
    <html lang="en" dir="ltr">
      <head>
        <meta charset="utf-8">
-       <title></title>
+       <title>Home</title>
      </head>
      <body>
+       <h1>Hello, <?php echo $_SESSION['user_name']; ?></h1>
+       <a href="logout.php">Logout</a>
 
      </body>
    </html>
+<?php
 }
+
+else {
+  header("Location: index.php");
+  exit();
+}
+?>
