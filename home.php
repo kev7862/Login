7e2978852c9989f1f6093b39fef7f9ee2f40e192
kev7862 <kevmatite7862@gmail.com>
2021-09-21 16:55:27 +0300

    Connect login.php with db, validating input fields

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+   ?>
+
+   <!DOCTYPE html>
+   <!DOCTYPE html>
+   <html lang="en" dir="ltr">
+     <head>
+       <meta charset="utf-8">
+       <title></title>
+     </head>
+     <body>
+
+     </body>
+   </html>
+}
